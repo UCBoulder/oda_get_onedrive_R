49aaf3295074c24fcd09ca1998c121be8dda77cb
peter shaffery <petersh@ffery.org>
2022-05-16 14:29:28 -0600

    Creating get_onedrive

--- R/get_onedrive.R
+++ R/get_onedrive.R
@@ -1,10 +1,10 @@
 get_onedrive = function() {
   sys = unname(Sys.info()["sysname"])
-  
+
   if (sys %in% c("Linux","Windows")) {}
   else {stop("Unrecognized operating system")}
 
-  
+
   dplyr::case_when(
     sys == "Linux" ~ paste0("/mnt/c/Users/",unname(Sys.info()["user"]),"/OneDrive - UCB-O365"),
     sys == "Windows" ~ Sys.getenv("OneDrive"),
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(onedriveR)
+
+test_check("onedriveR")
